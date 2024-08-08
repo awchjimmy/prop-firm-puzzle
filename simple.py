09e24aea23a4263715f8ac76c7b37ffe9ec77da5
awchjimmy <awchjimmy@gmail.com>
2024-08-08 10:03:21 +0800

    add two workflows

--- simple.py
+++ simple.py
@@ -15,6 +15,7 @@ def login():
     wait_until(Text('BTC/USD').exists, timeout_secs=10)
 
 def open_long():
+    # locate "BTC/USD" in watchlist view
     sleep(1)
     rightclick(Text('BTC/USD'))
 
@@ -24,32 +25,49 @@ def open_long():
     sleep(1)
     click(Text('Send Order'))
 
+    # wait for the popup message
     sleep(3)
 
 
 def close_long():
+    # locate "-" in position view
     sleep(1)
     rightclick(Text('—'))
 
     sleep(1)
     click(Text('Close Position'))
 
+    # wait for the popup message
     sleep(3)
 
-def main():
+def tradingview_trigger_buy():
     # start browser
-    start_chrome('https://trade.brightfunded.com/')
+    start_chrome('https://trade.brightfunded.com/', headless=True)
 
     # try to login
     login()
 
     # open long
-    # open_long()
+    open_long()
+
+    # stop browser
+    kill_browser()
+
+def tradingview_trigger_sell():
+    # start browser
+    start_chrome('https://trade.brightfunded.com/', headless=True)
+
+    # try to login
+    login()
 
     # close long
     close_long()
 
     # stop browser
     kill_browser()
+
+def main():
+    tradingview_trigger_buy()
+    tradingview_trigger_sell()
 
 main()
